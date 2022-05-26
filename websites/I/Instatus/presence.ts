1b51c3c7f899c9464c3d060765d7a162ced66c84
Mathieu <80390413+mccoderpy@users.noreply.github.com>
2022-05-26 23:51:53 +0200

    update(Instatus): make code performanter

--- websites/I/Instatus/presence.ts
+++ websites/I/Instatus/presence.ts
@@ -1,15 +1,68 @@
 const presence = new Presence({
-	clientId: "977756675647897650"
+	clientId: "977756675647897650",
+	injectOnComplete: true
 });
 
-let browsingTimestamp = Math.floor(Date.now() / 1000);
-let prevUrl = document.location.href;
+let browsingTimestamp = Math.floor(Date.now() / 1000),
+	prevUrl = document.location.href,
+	smallImageKeys: { [name: string]: string } = {
+		UP: "status-ok",
+		HASISSUES: "status-down",
+		UNDERMAINTENANCE: "status-notice"
+	};
+let { endTimestamp, ...currentPresenceData }: PresenceData = {};
 
-let smallImageKeys: { [name: string]: string } = {
-	UP: "status-ok",
-	HASISSUES: "status-down",
-	UNDERMAINTENANCE: "status-notice"
-};
+function defaultStatus(presenceData: PresenceData): PresenceData {
+	try {
+		presenceData.largeImageKey = document
+			.querySelector("div.z-20.large-header__logo img")
+			.getAttribute("src")
+			.replace(".svg", ".png");
+	} catch (error) {
+		presenceData.largeImageKey = presenceData.largeImageKey;
+	}
+	// using this way so we can add other subpages later easily //
+	const statics: {
+		[name: string]: PresenceData;
+	} = {
+		"/": {
+			details: `Viewing ${document.querySelector("head>title").textContent}`,
+			state: "Main page",
+			smallImageKey:
+				smallImageKeys[
+					document
+						.querySelector("#__NEXT_DATA__")
+						.textContent.match(
+							/\"status\"\:\"(?<status>UP|HASISSUES|UNDERMAINTENANCE)/
+						)
+						.groups.status.valueOf()
+				],
+			smallImageText: document.querySelector(".main-status__wrapper>div>div>h2")
+				.textContent,
+			buttons: [
+				{
+					label: "View status",
+					url: `${document.location.href}`
+				}
+			]
+		}
+	};
+	for (const [k, v] of Object.entries(statics)) {
+		if (
+			location.href
+				.replace(/\/?$/, "/")
+				.replace(`https://${document.location.hostname}`, "")
+				.replace("?", "/")
+				.replace("=", "/")
+				.match(k)
+		)
+			presenceData = { ...presenceData, ...v };
+	}
+	return presenceData;
+}
+
+presence.success("Succesful loaded presence");
+presence.error("You have not much sex");
 
 presence.on("UpdateData", async () => {
 	const [showTimestamp, showButtons, logo] = await Promise.all([
@@ -37,68 +90,10 @@ presence.on("UpdateData", async () => {
 		prevUrl = document.location.href;
 		browsingTimestamp = Math.floor(Date.now() / 1000);
 	}
-	switch (document.location.hostname) {
+	let hostname = document.location.hostname;
+	switch (hostname) {
 		case "instat.us": {
-			let pageHeader = document.querySelector("head>title").textContent;
-			let pageStatus = document.querySelector(
-				".main-status__wrapper>div>div>h2"
-			).textContent;
-			const statics: {
-				[name: string]: PresenceData;
-			} = {
-				"/": {
-					details: `Viewing ${pageHeader}`,
-					state: `${pageStatus}`,
-					smallImageKey:
-						smallImageKeys[
-							document
-								.querySelector("#__NEXT_DATA__")
-								.textContent.match(
-									`"status":"(?<status>UP|HASISSUES|UNDERMAINTENANCE)"`
-								)
-								.groups["status"].valueOf()
-						],
-					smallImageText: pageStatus,
-					buttons: [
-						{
-							label: `View status`,
-							url: `${prevUrl}`
-						}
-					]
-				},
-				"/history/": {
-					details: "Viewing Instatus status history",
-					state: `${pageHeader} ${pageStatus}`,
-					smallImageKey:
-						smallImageKeys[
-							document
-								.querySelector("#__NEXT_DATA__")
-								.textContent.match(
-									`"status":"(?<status>UP|HASISSUES|UNDERMAINTENANCE)"`
-								)
-								.groups["status"].valueOf()
-						],
-					smallImageText: pageStatus,
-					buttons: [
-						{
-							label: `View status history`,
-							url: `${prevUrl}`
-						}
-					]
-				}
-			};
-			for (const [k, v] of Object.entries(statics)) {
-				if (
-					location.href
-						.replace(/\/?$/, "/")
-						.replace(`https://${document.location.hostname}`, "")
-						.replace("?", "/")
-						.replace("=", "/")
-						.match(k)
-				) {
-					presenceData = { ...presenceData, ...v };
-				}
-			}
+			presenceData = defaultStatus(presenceData);
 			break;
 		}
 
@@ -117,10 +112,13 @@ presence.on("UpdateData", async () => {
 				},
 				"/help": {
 					details: "Reading the documentation",
-					state: `${location.href
-						.replace(/https:\/\/instatus.com\/help\/?(api\/?)?/, "")
-						.replace("/", " > ")
-						.replace("-", " ")}`,
+					state:
+						`${location.href
+							.replace(/https:\/\/instatus.com\/help\/?(api\/?)?/, "")
+							.replace("/", " > ")
+							.replace("-", " ")}` || "Main page",
+					smallImageKey: "reading",
+					smallImageText: "Reading the documentation",
 					buttons: [
 						{
 							label: "Open documentation",
@@ -129,7 +127,8 @@ presence.on("UpdateData", async () => {
 					]
 				},
 				"/help/api/": {
-					details: "Reading the api documentation"
+					details: "Reading the API documentation",
+					smallImageText: "Reading the API documentation"
 				}
 			};
 			for (const [k, v] of Object.entries(statics)) {
@@ -140,9 +139,8 @@ presence.on("UpdateData", async () => {
 						.replace("?", "/")
 						.replace("=", "/")
 						.match(k)
-				) {
+				)
 					presenceData = { ...presenceData, ...v };
-				}
 			}
 			break;
 		}
@@ -188,67 +186,30 @@ presence.on("UpdateData", async () => {
 						.replace("?", "/")
 						.replace("=", "/")
 						.match(k)
-				) {
+				)
 					presenceData = { ...presenceData, ...v };
-				}
 			}
 			break;
 		}
 		default: {
-			try {
-				presenceData.largeImageKey = document
-					.querySelector("div.z-20.large-header__logo img")
-					.getAttribute("src")
-					.replace(".svg", ".png");
-			} catch (error) {
-				presenceData.largeImageKey = presenceData.largeImageKey;
-			}
-			let pageHeader = document.querySelector("head>title").textContent;
-			let pageStatus = document.querySelector(
-				".main-status__wrapper>div>div>h2"
-			).textContent;
-			const statics: {
-				[name: string]: PresenceData;
-			} = {
-				"/": {
-					details: `Viewing ${pageHeader}`,
-					state: "Main page",
-					smallImageKey:
-						smallImageKeys[
-							document
-								.querySelector("#__NEXT_DATA__")
-								.textContent.match(
-									`"status":"(?<status>UP|HASISSUES|UNDERMAINTENANCE)"`
-								)
-								.groups["status"].valueOf()
-						],
-					smallImageText: pageStatus,
-					buttons: [
-						{
-							label: `View status`,
-							url: `${document.location.href}`
-						}
-					]
-				}
-			};
-			for (const [k, v] of Object.entries(statics)) {
-				if (
-					location.href
-						.replace(/\/?$/, "/")
-						.replace(`https://${document.location.hostname}`, "")
-						.replace("?", "/")
-						.replace("=", "/")
-						.match(k)
-				) {
-					presenceData = { ...presenceData, ...v };
-				}
-			}
+			presenceData = defaultStatus(presenceData);
 			break;
 		}
 	}
-	if (!presenceData.buttons?.length) delete presenceData.buttons;
-	if (!showButtons) delete presenceData.buttons;
+
+	if (!presenceData.buttons?.length || !showButtons)
+		delete presenceData.buttons;
 	if (showTimestamp) presenceData.startTimestamp = browsingTimestamp;
-	if (presenceData.details) presence.setActivity(presenceData);
-	else presence.setActivity();
+
+	if (presenceData.details) {
+		if (JSON.stringify(presenceData) != JSON.stringify(currentPresenceData)) {
+			currentPresenceData = { endTimestamp, ...presenceData };
+			try {
+				presence.setActivity(presenceData);
+				presence.info("Updated presence");
+			} catch (error) {
+				presence.error(error);
+			}
+		}
+	} else presence.setActivity();
 });
